bb90b747a09193e69f11beed256e850e1ab36f65
Jason Park <binarydiver@gmail.com>
2018-10-17 23:29:00 +0900

    Initialize Xcode project.

---
+++ prototype-vpn-macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  prototype-vpn-macos
+//
+//  Created by Jason Park on 17/10/2018.
+//  Copyright © 2018 Jason Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ prototype-vpn-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  prototype-vpn-macos
+//
+//  Created by Jason Park on 17/10/2018.
+//  Copyright © 2018 Jason Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
