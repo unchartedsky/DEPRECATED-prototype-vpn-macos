3179d08f7ae01501c19b93b1c4a4500c50e14700
Jason Park <binarydiver@gmail.com>
2018-10-20 22:44:19 +0900

    Add countryTable and show name of selected country.

--- prototype-vpn-macos/VpnViewController.swift
+++ prototype-vpn-macos/VpnViewController.swift
@@ -8,8 +8,12 @@
 
 import Cocoa
 
-class VpnViewController: NSViewController {
+class VpnViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var countryLabel: NSTextField!
+    
+    let countries = ["USA", "Japan", "Hongkong", "Canada", "Germany", "France", "Spain", "Singapore", "Brazil", "India"]
+    
     class func loadFromNib() -> VpnViewController {
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         return storyboard.instantiateController(withIdentifier: "VpnViewController") as! VpnViewController
@@ -20,4 +24,18 @@ class VpnViewController: NSViewController {
         // Do view setup here.
     }
     
+    // MARK: Data Source
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return countries[row]
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        if let tableView = notification.object {
+            countryLabel.stringValue = countries[(tableView as! NSTableView).selectedRow]
+        }
+    }
 }
